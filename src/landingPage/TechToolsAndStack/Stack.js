bc55320f70ec1518261499799996d82485260c9d
dhammabhushan <dhammwagh922004@gmail.com>
2024-10-22 23:58:58 +0530

    tech stack done ,tools

--- src/landingPage/TechToolsAndStack/Stack.js
+++ src/landingPage/TechToolsAndStack/Stack.js
@@ -1,13 +1,43 @@
 import React from 'react';
-import './tech.css'
-
+import { motion } from 'framer-motion';
+import './tech.css';
 
 function Stack() {
     return ( 
-            <div className='Stack-container'>
+        <motion.div
 
+        drag
+        dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+           initial={{x:-200,opacity:0}}
+           animate={{x:0,opacity:1}}
+           transition={{duration:0.5,delay:0.5}}
+            className='Stack-container'
+        >
+            <div className='tech-stack'>
+                <h2>TECH STACK</h2>
+
+                <div className='frontend-section'>
+                    <p>Frontend:</p>
+                    <button className='btn'>React</button>
+                    <button className='btn'>Framer Motion</button>
+                    <button className='btn'>HTML</button>
+                    <button className='btn'>Bootstrap</button>
+                </div>
+
+                <div className='Database-section'>
+                    <p>Database:</p>
+                    <button className='btn'>MySQL</button>
+                    <button className='btn'>MongoDB</button>
+                </div>
+
+                <div className='Backend-section'>
+                    <p>Backend:</p>
+                    <button className='btn'>Express</button>
+                    <button className='btn'>Node.js</button>
+                </div>
             </div>
-     );
+        </motion.div>
+    );
 }
 
 export default Stack;
--- src/landingPage/TechToolsAndStack/Tools.js
+++ src/landingPage/TechToolsAndStack/Tools.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+import './tech.css'; 
 
 function Tools() {
-    return ( 
-
-       <div className='Tools-container'></div>
-     );
+    return (
+        <motion.div
+            drag
+            dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+            initial={{ x: -200, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.7, delay: 0.5 }}
+            className='Tools-container'
+        >
+            <div className='tools'>
+                <motion.div className='tool-item'><img src="/vs-code.webp" alt="Visual Studio Code" /></motion.div>
+                <motion.div className='tool-item'><img src="/chrome.svg" alt="Google Chrome" /></motion.div>
+                <motion.div className='tool-item-gpt'><img src="/gpt.svg" alt="ChatGPT" /></motion.div>
+                <motion.div className='tool-item'><img src='/framer.svg' alt='Framer' /></motion.div>
+ 
+            </div>
+        </motion.div>
+    );
 }
 
 export default Tools;
