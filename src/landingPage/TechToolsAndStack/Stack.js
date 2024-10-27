9357c86141050997b3132df3e04720ac3ff63be8
dhammabhushan <dhammwagh922004@gmail.com>
2024-10-27 17:49:04 +0530

    add change in tech stack section

--- src/landingPage/TechToolsAndStack/Stack.js
+++ src/landingPage/TechToolsAndStack/Stack.js
@@ -2,40 +2,43 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import './tech.css';
 
+function TechButton({ children }) {
+    return (
+        <div className='btn'>{children}</div>
+    );
+}
+
 function Stack() {
     return ( 
         <motion.div
-
-        drag
-        dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
-           initial={{x:-200,opacity:0}}
-           animate={{x:0,opacity:1}}
-           transition={{duration:0.5,delay:0.5}}
+            drag
+            dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+            initial={{ x: -200, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.5, delay: 0.5 }}
             className='Stack-container'
         >
             <div className='tech-stack'>
                 <h2>TECH STACK</h2>
 
-
                 <p>Frontend:</p>
                 <div className='frontend-section'>
-                    
-                    <div className='btn'>React</div>
-                    <div className='btn'>Framer Motion</div>
-                    <div className='btn'>HTML</div>
-                    <div className='btn'>Bootstrap</div>
+                    <TechButton>React</TechButton>
+                    <TechButton>Framer Motion</TechButton>
+                    <TechButton>HTML</TechButton>
+                    <TechButton>Bootstrap</TechButton>
                 </div>
 
+                <p>Database:</p>
                 <div className='Database-section'>
-                    <p>Database:</p>
-                    <div className='btn'>MySQL</div>
-                    <div className='btn'>MongoDB</div>
+                    <TechButton>MySQL</TechButton>
+                    <TechButton>MongoDB</TechButton>
                 </div>
 
+                <p>Backend:</p>
                 <div className='Backend-section'>
-                    <p>Backend:</p>
-                    <div className='btn'>Express</div>
-                    <div className='btn'>Node.js</div>
+                    <TechButton>Express</TechButton>
+                    <TechButton>Node.js</TechButton>
                 </div>
             </div>
         </motion.div>
