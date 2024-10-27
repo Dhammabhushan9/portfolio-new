b8755e4aa6615a2e94c849fee839973ef898e890
dhammabhushan <dhammwagh922004@gmail.com>
2024-10-27 22:46:56 +0530

    Adding media query and change the order

--- src/App.js
+++ src/App.js
@@ -1,34 +1,41 @@
- 
 import './App.css';
-import Tools from './landingPage/TechToolsAndStack/TechHero'
-import About from './landingPage/AboutSection/About'
-import ProjectAndExp from './landingPage/ProjectAndExpSection/Hero'
+import Tools from './landingPage/TechToolsAndStack/TechHero';
+import About from './landingPage/AboutSection/About';
+import ProjectAndExp from './landingPage/ProjectAndExpSection/Hero';
 import Project_section from './landingPage/ProjectAndExpSection/Project_section';
 import Experience_section from './landingPage/ProjectAndExpSection/Experience_section';
 import Certification_section from './landingPage/TechToolsAndStack/Certification_section';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/project' element={<Project_section/>}/>
-          <Route path='/experience' element={<Experience_section/>}/>
-          <Route path='/certification' element={<Certification_section/>}/>
-      </Routes>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/project' element={<Project_section />} />
+          <Route path='/experience' element={<Experience_section />} />
+          <Route path='/certification' element={<Certification_section />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
 }
-function Home(){
-  return<>
-  
-  <Tools/>
-  <About/>
-  <ProjectAndExp/>
-  </>
-}
 
+function Home() {
+  return (
+    <div className="home-container">
+      <div className="tools">
+        <Tools />
+      </div>
+      <div className="about">
+        <About />
+      </div>
+      <div className="project-and-exp">
+        <ProjectAndExp />
+      </div>
+    </div>
+  );
+}
 
 export default App;
